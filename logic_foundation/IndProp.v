c9afb9aace4a4d19eb4660b2ac49ce29578b4785
Kailiang Ji <kailiang.ji@icloud.com>
2020-03-18 00:20:46 +0800

    amend IndProp

--- logic_foundation/IndProp.v
+++ logic_foundation/IndProp.v
@@ -90,3 +90,63 @@ Proof.
       rewrite H'. apply even'_sum. constructor. apply IHeven.
 Qed.
 
+Theorem ev_ev__ev : forall n m,
+    even (n + m) -> even n -> even m.
+Proof.
+  intros n m H1 H2. induction H2 as [| n' H2' IH].
+  - simpl in H1. apply H1.
+  - apply IH. simpl in H1. inversion H1. apply H0.
+Qed.
+
+Theorem ev_plus_plus : forall n m p,
+    even (n + m) -> even (n + p) -> even (m + p).
+Proof.
+  intros n m p H1 H2.
+  apply (ev_ev__ev (n+p) (m+p)).
+  assert (H: n + p + (m + p) = (n + m) + (p + p)).
+  { assert (H' : m + p = p + m).
+    { apply plus_comm. }
+    rewrite H'. rewrite plus_assoc. rewrite plus_comm.
+    assert (H'': n + p + p = n + (p + p)).
+    rewrite plus_assoc. reflexivity. rewrite H''.
+    rewrite plus_assoc. rewrite (plus_comm n m).
+    reflexivity. }
+  rewrite H. apply ev_sum. apply H1. Search double.
+  rewrite <- double_plus. apply ev_double.
+  apply H2.
+Qed.
+
+Module Playground.
+  Inductive le : nat -> nat -> Prop :=
+  | le_n n : le n n
+  | le_S n m (H : le n m) : le n (S m).
+
+  Notation "m <= n" := (le m n).
+
+  Example test_le1 : 3 <= 3.
+  Proof. apply (le_n 3). Qed.
+
+  Example test_le2 : 3 <= 6.
+  Proof.
+    apply (le_S 3 5). apply (le_S 3 4). apply (le_S 3 3).
+    apply (le_n 3).
+  Qed.
+
+  Example test_le3 : (2 <= 1) -> 2 + 2 = 5.
+  Proof.
+    intro H. inversion H. inversion H2.
+  Qed.
+
+End Playground.
+
+Definition lt (n m : nat) := le (S n) m.
+
+Inductive square_of : nat -> nat -> Prop :=
+| sq n : square_of n (n * n).
+
+Inductive next_nat : nat -> nat -> Prop :=
+| nn n : next_nat n (S n).
+
+Inductive next_even : nat -> nat -> Prop :=
+| ne_1 n : even (S n) -> next_even n (S n)
+| ne_2 n (H : even (S (S n))) : next_even n (S (S n)).
