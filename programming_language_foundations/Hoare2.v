7495e1cd866389ebb59a100951024775bf389353
Kailiang Ji <kailiang.ji@icloud.com>
2020-07-05 00:53:51 +0800

    Hoare2.v

---
+++ programming_language_foundations/Hoare2.v
@@ -0,0 +1,12 @@
+Set Warnings "-notation-overridden,-parsing".
+Add LoadPath "/Users/jikl/demos/coq/software-foundation-exercise/logic_foundation" as LF.
+Add LoadPath "/Users/jikl/demos/coq/software-foundation-exercise/programming_language_foundations" as PLF.
+From Coq Require Import Strings.String.
+From LF Require Import Maps.
+From Coq Require Import Bool.Bool.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import Arith.PeanoNat. Import Nat.
+From Coq Require Import omega.Omega.
+From PLF Require Import Hoare.
+From PLF Require Import Imp.
