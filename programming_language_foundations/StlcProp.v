ee360c8c731b54a8b01b30a00edd2389c2887d26
Kailiang Ji <kailiang.ji@icloud.com>
2020-08-22 23:51:57 +0800

    update StlcProp.v

--- programming_language_foundations/StlcProp.v
+++ programming_language_foundations/StlcProp.v
@@ -102,4 +102,76 @@ Module STLCProp.
         * destruct H3. eexists. apply ST_Test. apply H3.
   Qed.
 
+  Inductive appears_free_in : string -> tm -> Prop :=
+  | afi_var : forall x,
+      appears_free_in x (var x)
+  | afi_app1 : forall x t1 t2,
+      appears_free_in x t1 ->
+      appears_free_in x (app t1 t2)
+  | afi_app2 : forall x t1 t2,
+      appears_free_in x t2 ->
+      appears_free_in x (app t1 t2)
+  | afi_abs : forall x y T11 t12,
+      y <> x ->
+      appears_free_in x t12 ->
+      appears_free_in x (abs y T11 t12)
+  | afi_test1 : forall x t1 t2 t3 ,
+      appears_free_in x t1 ->
+      appears_free_in x (test t1 t2 t3)
+  | afi_test2 : forall x t1 t2 t3,
+      appears_free_in x t2 ->
+      appears_free_in x (test t1 t2 t3)
+  | afi_test3 : forall x t1 t2 t3,
+      appears_free_in x t3 ->
+      appears_free_in x (test t1 t2 t3).
   
+  Hint Constructors appears_free_in : db.
+
+  Definition closed (t : tm) :=
+    forall x, ~ appears_free_in x t.
+
+  Lemma free_in_context : forall x t T Gamma,
+      appears_free_in x t ->
+      Gamma |- t ? T ->
+      exists T', Gamma x = Some T'.
+  Proof.
+    intros x t T Gamma Hafi.
+    generalize dependent Gamma. generalize dependent T.
+    induction Hafi.
+    - intros T Gamma H. inversion H; subst. eapply ex_intro. apply H2.
+    - intros T Gamma H. inversion H; subst.
+      eapply IHHafi. apply H3.
+    - intros T Gamma H. inversion H; subst. eapply IHHafi. apply H5.
+    - intros T Gamma H1. inversion H1; subst.
+      apply (IHHafi T12 (y0 |-> T11; Gamma)) in H6.
+      destruct H6. rewrite update_neq in H0.
+      + eexists. apply H0.
+      + apply H.
+    - intros T Gamma H. inversion H; subst.
+      eapply IHHafi. apply H4.
+    - intros T Gamma H. inversion H; subst.
+      eapply IHHafi. apply H6.
+    - intros T Gamma H. inversion H; subst.
+      eapply IHHafi. apply H7.
+  Qed.
+
+  Corollary typable_empty__closed : forall t T,
+      empty |- t ? T ->
+      closed t.
+  Proof.
+    unfold closed. intros t T H x0 H1.
+    apply (free_in_context x0 t T empty H1) in H.
+    inversion H. inversion H0.
+  Qed.
+
+  Lemma context_invariance : forall Gamma Gamma' t T,
+      Gamma |- t ? T ->
+      (forall x, appears_free_in x t -> Gamma x = Gamma' x) ->
+      Gamma' |- t ? T.
+  Proof.
+   (* intros Gamma Gamma' t T H1 H2.
+    induction H1.
+    - apply T_Var. rewrite <- H. symmetry. eapply H2. apply afi_var.
+    - apply T_Abs. 
+    *)
+    Admitted.
